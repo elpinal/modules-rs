9f5bec0f8be8a495e1dcca73c8a5ef0208a9a2ed
elpinal <elpinal@users.noreply.github.com>
2019-02-11 07:52:12 +0900

    Add Type::some

--- src/rrd2014/internal.rs
+++ src/rrd2014/internal.rs
@@ -2,22 +2,28 @@
 
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Name(String);
 
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum Label {
     Label(Name),
 }
 
-struct Variable(usize);
+#[derive(Debug, PartialEq)]
+pub struct Variable(usize);
 
-struct Record<T>(HashMap<Label, T>);
+#[derive(Debug, PartialEq)]
+pub struct Record<T>(HashMap<Label, T>);
 
-enum Kind {
+#[derive(Debug, PartialEq)]
+pub enum Kind {
     Mono,
     Fun(Box<Kind>, Box<Kind>),
 }
 
-enum Type {
+#[derive(Debug, PartialEq)]
+pub enum Type {
     Var(Variable),
     Fun(Box<Type>, Box<Type>),
     Record(Record<Type>),
@@ -39,4 +45,48 @@ enum Term {
     Pack(Type, Box<Term>, Type),
     Unpack(Box<Term>, Box<Term>),
     Int(isize),
+}
+
+impl Kind {
+    pub fn fun(k1: Kind, k2: Kind) -> Self {
+        Kind::Fun(Box::new(k1), Box::new(k2))
+    }
+}
+
+impl Type {
+    /// Creates an n-ary existential type.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use modules::rrd2014::internal::*;
+    /// use Type::Int;
+    /// use Kind::*;
+    ///
+    /// assert_eq!(Type::some(None, Int), Int);
+    ///
+    /// assert_eq!(
+    ///     Type::some(vec![Mono], Int),
+    ///     Type::Some(Mono, Box::new(Int))
+    /// );
+    ///
+    /// assert_eq!(
+    ///     Type::some(vec![Mono, Mono], Int),
+    ///     Type::Some(Mono, Box::new(Type::Some(Mono, Box::new(Int))))
+    /// );
+    ///
+    /// assert_eq!(
+    ///     Type::some(vec![Kind::fun(Mono, Mono), Mono], Int),
+    ///     Type::Some(Mono, Box::new(Type::Some(Kind::fun(Mono, Mono), Box::new(Int))))
+    /// );
+    /// ```
+    pub fn some<I>(ks: I, mut ty: Type) -> Self
+    where
+        I: IntoIterator<Item = Kind>,
+    {
+        for k in ks.into_iter() {
+            ty = Type::Some(k, Box::new(ty))
+        }
+        ty
+    }
 }
