4e6eb9f698d62ce012035bf980b6d64dcafdcd1c
elpinal <elpinal@users.noreply.github.com>
2019-02-11 07:37:07 +0900

    Define the syntax of the internal language

--- src/rrd2014.rs
+++ src/rrd2014.rs
@@ -2,4 +2,6 @@
 //! F-ing modules.
 //! Journal of Functional Programming, 24(5), 2014.
 
+#![allow(dead_code)]
+
 pub mod internal;
--- src/rrd2014/internal.rs
+++ src/rrd2014/internal.rs
@@ -1,1 +1,42 @@
 //! The internal language.
+
+use std::collections::HashMap;
+
+struct Name(String);
+
+enum Label {
+    Label(Name),
+}
+
+struct Variable(usize);
+
+struct Record<T>(HashMap<Label, T>);
+
+enum Kind {
+    Mono,
+    Fun(Box<Kind>, Box<Kind>),
+}
+
+enum Type {
+    Var(Variable),
+    Fun(Box<Type>, Box<Type>),
+    Record(Record<Type>),
+    Forall(Kind, Box<Type>),
+    Some(Kind, Box<Type>),
+    Abs(Kind, Box<Type>),
+    App(Box<Type>, Box<Type>),
+    Int,
+}
+
+enum Term {
+    Var(Variable),
+    Abs(Type, Box<Term>),
+    App(Box<Term>, Box<Term>),
+    Record(Record<Term>),
+    Proj(Box<Term>, Label),
+    Poly(Kind, Box<Term>),
+    Inst(Box<Term>, Type),
+    Pack(Type, Box<Term>, Type),
+    Unpack(Box<Term>, Box<Term>),
+    Int(isize),
+}
