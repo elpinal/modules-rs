c156a7e72c8a8f7cb7618c65592ccaac0da64f2c
elpinal <elpinal@users.noreply.github.com>
2019-02-11 18:01:05 +0900

    Define the syntax of the external language

--- src/rrd2014.rs
+++ src/rrd2014.rs
@@ -5,3 +5,62 @@
 #![allow(dead_code)]
 
 pub mod internal;
+
+use internal::Name;
+
+struct Ident(Name);
+
+enum Kind {
+    Mono,
+}
+
+enum Type {
+    Fun(Box<Type>, Box<Type>),
+    Path(Path),
+    Int,
+}
+
+enum Expr {
+    Var(Ident),
+    Abs(Ident, Box<Expr>),
+    App(Box<Expr>, Box<Expr>),
+    Path(Path),
+    Int(isize),
+}
+
+struct Path(Box<Module>);
+
+enum Module {
+    Ident(Ident),
+    Seq(Vec<Binding>),
+    Proj(Box<Module>, Ident),
+    Fun(Ident, Sig, Box<Module>),
+    App(Ident, Ident),
+    Seal(Ident, Sig),
+}
+
+enum Binding {
+    Val(Ident, Expr),
+    Type(Ident, Type),
+    Module(Ident, Module),
+    Signature(Ident, Sig),
+    Include(Module),
+}
+
+enum Sig {
+    Path(Path),
+    Seq(Vec<Decl>),
+    Fun(Ident, Box<Sig>, Box<Sig>),
+    Where(Box<Sig>, Proj<Ident>, Type),
+}
+
+struct Proj<T>(T, Vec<T>);
+
+enum Decl {
+    Val(Ident, Type),
+    ManType(Ident, Type),
+    AbsType(Ident, Kind),
+    Module(Ident, Sig),
+    Signature(Ident, Sig),
+    Include(Sig),
+}
