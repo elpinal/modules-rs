8ff178aad62e29222dcb42541f74c849c3bd54bf
elpinal <elpinal@users.noreply.github.com>
2019-03-01 11:34:06 +0900

    Add an integration test

---
+++ tests/integration.rs
@@ -0,0 +1,21 @@
+use modules::exec;
+
+macro_rules! assert_exec {
+    ($s:expr) => {{
+        let r = exec($s.chars());
+        assert!(r.is_ok(), "{:?}", r.unwrap_err());
+    }};
+}
+
+#[test]
+fn test_execution() {
+    assert_exec!("struct end");
+    assert_exec!("struct val x = 1 end");
+
+    assert_exec!(
+        "struct
+         val x = 1
+         val y = 1
+         end"
+    );
+}
